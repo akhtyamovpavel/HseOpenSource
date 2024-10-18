7fa4d0a1861ed4579a3c7ee60e43fe13e62ef03f
Pavel Akhtyamov <akhtyamovpavel@gmail.com>
2024-10-18 19:36:21 +0300

    Refactored code

--- triangle/triangle.py
+++ triangle/triangle.py
@@ -1,17 +1,24 @@
+def check_triangle(a, b, c):
+    if not isinstance(a, (int, float)) \
+            or not isinstance(b, (int, float)) \
+            or not isinstance(c, (int, float)):
+        return False
+
+    if a <= 0 or b <= 0 or c <= 0:
+        return False
+
+    if a + b <= c or b + c <= a or a + c <= b:
+        return False
+
+    return True
+
+
 def find_triangle(*args):
     a = args[0]
     b = args[1]
     c = args[2]
 
-    if not isinstance(a, (int, float)) \
-            or not isinstance(b, (int, float)) \
-            or not isinstance(c, (int, float)):
-        return 'bad'
-
-    if a <= 0 or b <= 0 or c <= 0:
-        return 'bad'
-
-    if a + b <= c or b + c <= a or a + c <= b:
+    if not check_triangle(a, b, c):
         return 'bad'
 
     if a == b and b == c:
